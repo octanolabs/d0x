b5370d5d27c0bae05c442a3b8914745964bd2b7c
Luke Williams <luke@iquidus.io>
2019-09-28 16:27:40 +0200

    axios example: implement copy to clipboard

--- src/components/examples/Axios.vue
+++ src/components/examples/Axios.vue
@@ -15,21 +15,47 @@
         </template>
         <span>Refresh</span>
       </v-tooltip>
+
       <v-tooltip
         left
       >
         <template v-slot:activator="{ on }">
-          <v-btn icon color="primary" v-on="on">
+          <v-btn icon color="primary" v-on="on" v-clipboard:copy="format()" v-clipboard:success="copySuccess" v-clipboard:error="copyError">
             <v-icon small>mdi-content-copy</v-icon>
           </v-btn>
         </template>
         <span>Copy to clipboard</span>
       </v-tooltip>
+
     </v-card-actions>
     <v-divider />
     <v-card-text class="pa-0">
       <pre v-highlightjs="format()"><code class="javascript w-100 elevation-0"></code></pre>
     </v-card-text>
+    <v-snackbar
+      v-model="snackCopySuccess"
+      color="primary"
+    >
+      Example code has been copied to your clipboard.
+      <v-btn
+        text
+        @click="snackCopySuccess = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+    <v-snackbar
+      v-model="snackCopyError"
+      color="error"
+    >
+      Unable to copy to clipboard. Copy manually or change browser.
+      <v-btn
+        text
+        @click="snackCopyError = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
   </v-card>
 </template>
 
@@ -79,7 +105,9 @@ export default {
   },
   data () {
     return {
-      randParams: []
+      randParams: [],
+      snackCopySuccess: false,
+      snackCopyError: false
     }
   },
   methods: {
@@ -138,6 +166,12 @@ export default {
     },
     refreshParams () {
       this.randParams = this.exampleParams()
+    },
+    copySuccess () {
+      this.snackCopySuccess = true
+    },
+    copyError () {
+      this.snackCopyError = true
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './layouts/default.vue'
 import VueHighlightJS from 'vue-highlightjs'
+import VueClipboard from 'vue-clipboard2'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import md from './plugins/markdown-it';
 
-
 Vue.config.productionTip = false
 Vue.use(VueHighlightJS)
+
+VueClipboard.config.autoSetContainer = true // modal fix
+Vue.use(VueClipboard)
 
 // inject markdown-it globally as this.$md
 Vue.mixin({
