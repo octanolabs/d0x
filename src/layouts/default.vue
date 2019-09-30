199a4e21c730a9e57385c51c46aa5ff4ae1dbc5e
Luke Williams <luke@iquidus.io>
2019-10-01 01:52:03 +0200

    remove empty submenu section

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -9,21 +9,6 @@
       floating
     >
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-
         <v-subheader
           v-if="!miniVariant"
         >
