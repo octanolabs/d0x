3b558f7b01f32d43e561301400d661779535915a
Luke Williams <luke@iquidus.io>
2019-10-14 23:34:38 +0200

    Viewer: only show methods

--- src/views/Openrpc.vue
+++ src/views/Openrpc.vue
@@ -1,36 +1,9 @@
 <template>
   <v-layout>
     <v-flex class="text-center">
-      <openrpc-info-bar :info="openrpc.info" :endpoint="endpoint"/>
       <v-sheet style="width:100%; overflow: hidden;">
-        <v-tabs v-model="openrpcTab" grow>
-          <v-tab
-            v-for="tab in tabs"
-            :key="tab"
-            ripple
-          >
-            {{ tab }}
-          </v-tab>
-        </v-tabs>
-        <v-divider />
-        <v-tabs-items v-model="openrpcTab">
-          <v-tab-item key="Methods">
-            <openrpc-methods :data="openrpc.methods" :api="apiId"/>
-          </v-tab-item>
-          <v-tab-item key="Components">
-            <v-col class="d-flex" cols="12">
-              <v-card  v-if="openrpc.components" outlined>
-                <v-card-title class="align-end">Content Descriptors</v-card-title>
-                <v-flex
-                  v-for="(item, i) in openrpc.components.contentDescriptors"
-                  :key="i"
-                >
-                  <openrpc-param :item="item" />
-                </v-flex>
-              </v-card>
-            </v-col>
-          </v-tab-item>
-        </v-tabs-items>
+        <openrpc-info-bar :info="openrpc.info" :endpoint="endpoint"/>
+        <openrpc-methods :data="openrpc.methods" :api="apiId"/>
       </v-sheet>
     </v-flex>
   </v-layout>
@@ -39,7 +12,6 @@
 <script>
 import axios from 'axios'
 import $RefParser from 'json-schema-ref-parser'
-import OpenrpcParam from '@/components/cards/Param'
 import OpenrpcInfoBar from '@/components/bars/Info'
 import OpenrpcMethods from '@/components/tables/Methods'
 
@@ -47,8 +19,7 @@ export default {
   props: ['apiId'],
   components: {
     OpenrpcInfoBar,
-    OpenrpcMethods,
-    OpenrpcParam
+    OpenrpcMethods
   },
   watch: {
     apiId: function () {
@@ -64,8 +35,6 @@ export default {
       openrpc: {},
       jsonUrl: '',
       endpoint: '',
-      openrpcTab: null,
-      tabs: ['Methods', 'Components'],
       statusCopySuccess: false,
       statusCopyError: false
     }
