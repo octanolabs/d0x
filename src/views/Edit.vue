c247a0efe7a4a1d928de19dac9b0cddf337633b2
Luke Williams <luke@iquidus.io>
2019-10-15 22:53:01 +0200

    Editor: tidy up, restructure layout, copy to clipboard

--- src/views/Edit.vue
+++ src/views/Edit.vue
@@ -1,31 +1,37 @@
 <template>
   <v-layout>
+    <left-drawer :methods="methods"/>
     <v-flex>
       <v-sheet style="width:100%; overflow: hidden;">
         <v-flex>
-          <v-toolbar class="elevation-0">
-            <v-btn
-              icon
-              text
-              @click.stop="tab = 0"
-            >
-              <v-icon>mdi-json</v-icon>
-            </v-btn>
-            <v-btn
-              icon
-              text
-              @click.stop="tab = 1"
-            >
-              <v-icon>mdi-compare</v-icon>
-            </v-btn>
+          <v-toolbar dense color="#212121" class="elevation-0">
+            <v-tabs v-model="tab">
+              <v-tab ripple>
+                <v-icon small class="mr-1">mdi-json</v-icon>
+                Editor
+              </v-tab>
+              <v-tab ripple>
+                <v-icon small class="mr-1">mdi-compare</v-icon>
+                Diff
+              </v-tab>
+            </v-tabs>
             <v-spacer />
-            <v-btn
-              icon
-              text
-              @click.stop="tab = 1"
-            >
-              <v-icon>mdi-content-copy</v-icon>
-            </v-btn>
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  icon
+                  small
+                  v-on="on"
+                  v-clipboard:copy="modified"
+                  v-clipboard:success="copySuccess"
+                  v-clipboard:error="copyError"
+                >
+                  <v-icon small>mdi-content-copy</v-icon>
+                </v-btn>
+              </template>
+              <span>Copy to clipboard.</span>
+            </v-tooltip>
+
           </v-toolbar>
           <v-tabs-items v-model="tab">
             <v-tab-item key="Editor">
@@ -33,16 +39,48 @@
                 :options="editorOptions"
                 class="editor"
                 v-model="modified"
-                :theme="darkMode ? 'vs-dark' : 'vs'" language="json"
+                :theme="theme"
+                language="json"
               />
             </v-tab-item>
             <v-tab-item key="Diff">
-              <MonacoEditor :options="diffEditorOptions" :diffEditor="true" class="editor" v-model="modified" :original="original" :theme="darkMode ? 'vs-dark' : 'vs'" language="json"/>
+              <MonacoEditor
+                :options="diffEditorOptions"
+                :diffEditor="true"
+                class="editor"
+                v-model="modified"
+                :original="original"
+                :theme="theme"
+                language="json"/>
             </v-tab-item>
           </v-tabs-items>
         </v-flex>
       </v-sheet>
     </v-flex>
+    <v-snackbar
+      v-model="copy.success"
+      color="primary"
+    >
+      Copied to your clipboard.
+      <v-btn
+        text
+        @click="copy.success = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+    <v-snackbar
+      v-model="copy.error"
+      color="error"
+    >
+      Unable to copy to clipboard. Copy manually or change browser.
+      <v-btn
+        text
+        @click="copy.error = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
   </v-layout>
 </template>
 
@@ -50,11 +88,13 @@
 import axios from 'axios'
 import MonacoEditor from 'vue-monaco'
 import $RefParser from 'json-schema-ref-parser'
+import LeftDrawer from '@/components/LeftDrawer';
 
 export default {
   props: ['apiId'],
   components: {
-    MonacoEditor
+    MonacoEditor,
+    LeftDrawer
   },
   watch: {
     apiId: function () {
@@ -65,24 +105,22 @@ export default {
     this.init()
   },
   computed: {
-    code () {
-      return JSON.stringify(this.openrpc, null, 2)
+    theme () {
+      return this.$vuetify.theme.dark ? 'vs-dark' : 'vs'
     },
-    darkMode () {
-      return this.$vuetify.theme.dark
+    methods () {
+      return this.$store.state.methods
     }
   },
   data () {
     return {
-      errors: [],
-      openrpc: {},
       jsonUrl: '',
       endpoint: '',
       original: '',
       modified: '',
       tab: 0,
       editorOptions: {
-        automaticLayout: true,
+        automaticLayout: true, // polls parent container size every 100ms and resizes if changed.
         scrollBeyondLastLine: false,
       },
       diffEditorOptions: {
@@ -90,6 +128,10 @@ export default {
         ignoreCharChanges: true,
         scrollBeyondLastLine: false,
         automaticLayout: true
+      },
+      copy: {
+        success: false,
+        error: false
       }
     }
   },
@@ -105,15 +147,16 @@ export default {
         .then((r) => {
           console.log(r)
           if (r.data.openrpc) {
+            // orginal (unmodified) source openrpc.json
             this.original = JSON.stringify(r.data, null, 2)
+            // v-model used by editor (separate so we can use diff editor)
             this.modified  = this.original
 
-            // TODO
+            // de-ref openrpc document
             $RefParser.dereference(r.data, (err, schema) => {
               if (err) {
                 console.log(err)
               } else {
-                this.openrpc = schema
                 const schemas = schema.components.schemas
                 const methods = schema.methods
                 // add api endpoint to openrpc.info
@@ -138,6 +181,12 @@ export default {
         .catch((e) => {
           console.log(e)
         })
+    },
+    copySuccess () {
+      this.copy.success = true
+    },
+    copyError () {
+      this.copy.error = true
     }
   }
 }
