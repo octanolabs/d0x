f1e449c9ce2f35d00be0dd63fb10b28f6563bfd1
Luke Williams <luke@iquidus.io>
2019-10-25 22:35:31 +0200

    add additional img to home/about

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,17 +5,14 @@
       class="pa-6"
     >
       <h3>Welcome to Octano d0x</h3>
-      <p>d0x is an open-source <a href="https://open-rpc.org" target="_blank">OpenRPC</a> document viewer and editor written in <a href="https://vuejs.org/" target="_blank">Vue.js</a>.</p>
+      <p>d0x is an open-source <a href="https://open-rpc.org/beginners/" target="_blank">OpenRPC</a> document viewer and editor written in <a href="https://vuejs.org/" target="_blank">Vue.js</a>.</p>
+      <p>Select an API in the dropdown menu above or start on a <router-link :to="{name: 'Edit', params: {apiId: 'new'}}">new OpenRPC document</router-link>.</p>
       <v-card
         elevation="12"
         class="ma-auto"
         max-width="80%"
       >
-        <v-carousel
-          :show-arrows="false"
-          height="600px"
-          cycle
-        >
+        <v-carousel>
           <v-carousel-item
             v-for="(item,i) in items"
             :key="i"
@@ -46,7 +43,7 @@
 export default {
   data () {
     return {
-      items: ['1.png','2.png','3.png','4.png']
+      items: ['0.png', '1.png','2.png','3.png','4.png']
     }
   }
 }
