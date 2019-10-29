b4258170ec9b55fc4b8da226529a32eb2a55ceed
Luke Williams <luke@iquidus.io>
2019-10-29 11:17:58 +0100

    add publicPath to vue.config (gh-pages)

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,10 @@ module.exports = {
         data: '@import "./scss/style.scss"'
       }
     }
-  }
+  },
+  // gh-pages
+  // https://cli.vuejs.org/guide/deployment.html#pwa
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/d0x/'
+    : '/'
 };
