152c7d21953ae6699d73a158f13e1085807b3931
Luke Williams <luke@iquidus.io>
2019-10-29 12:06:37 +0100

    fix: update github link on home/about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,7 +6,7 @@
     >
       <h3>Welcome to Octano d0x</h3>
       <p>Octano d0x is an <a href="https://open-rpc.org/beginners/" target="_blank">OpenRPC</a> interface built using <a href="https://vuejs.org/" target="_blank">Vue.js</a>.</p>
-      <p>Select an API in the dropdown menu above, get started on a <router-link :to="{name: 'Edit', params: {apiId: 'custom'}}">new OpenRPC document</router-link>, or get involved on <a href="https://github.com/octanolabs/vue-openrpc-d0x" target="_blank">Github</a>.</p>
+      <p>Select an API in the dropdown menu above, get started on a <router-link :to="{name: 'Edit', params: {apiId: 'custom'}}">new OpenRPC document</router-link>, or get involved on <a href="https://github.com/octanolabs/d0x" target="_blank">Github</a>.</p>
 
       <v-carousel
         :show-arrows="false"
