c55d25718d67d92ac3bea9b235c6ee7665743027
Luke Williams <luke@iquidus.io>
2019-11-01 01:38:19 +0100

    move scss to @/scss

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,13 @@
 module.exports = {
   css: {
     loaderOptions: {
-      scss: {
-        data: '@import "./scss/style.scss"'
+      // by default the `sass` option will apply to both syntaxes
+      // because `scss` syntax is also processed by sass-loader underlyingly
+      // but when configuring the `data` option
+      // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
+      // in that case, we can target the `scss` syntax separately using the `scss` option
+      sass: {
+        data: `@import "@/scss/style.scss"`
       }
     }
   },
