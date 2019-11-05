eeed861da5a7ad98fa796c5cf0ce30243c2ccf72
Luke Williams <luke@iquidus.io>
2019-11-05 20:35:13 +0100

    sass: update setup for vuetify 2

--- vue.config.js
+++ vue.config.js
@@ -6,8 +6,8 @@ module.exports = {
       // but when configuring the `data` option
       // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
       // in that case, we can target the `scss` syntax separately using the `scss` option
-      scss: {
-        data: `@import "@/scss/style.scss";`
+      sass: {
+        implementation: require('sass')
       }
     }
   },
